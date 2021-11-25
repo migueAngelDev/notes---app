781e7c73ea6c4171bac6bff9d17f87d9e71cecfd
Miguel Angel <angel_ton618@hotmail.com>
2021-11-25 16:17:18 -0600

    Finished project

--- app.js
+++ app.js
@@ -8,5 +8,56 @@ $save.addEventListener("submit", (e) => {
     description: $description.value,
   };
 
+  if (localStorage.getItem("tasks") === null) {
+    let tasks = [];
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  } else {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  getTasks();
+
+  document.getElementById("formTask").reset();
+
   e.preventDefault();
 });
+
+function getTasks() {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  let tasksView = document.getElementById("tasks");
+
+  tasksView.innerHTML = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+
+    tasksView.innerHTML += `
+    <div class="card mb-4">
+      <div class="card-body">
+          <p>${title} - ${description}</p>
+             <a class="btn btn-danger" onclick="deleteTask('${title}')">
+             Delete
+             </a>
+      </div>
+    </div>`;
+  }
+}
+
+function deleteTask(title) {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].title == title) {
+      tasks.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  getTasks();
+}
+
+getTasks();
